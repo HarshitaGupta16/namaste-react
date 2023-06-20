66c2199bd101c0c239e10ac06bf487f9a17fbf6d
Harshita Gupta <sign.of.happiness@gmail.com>
2023-06-20 23:04:26 +0530

    Episode-06 Exploring the world - data fetching with swiggy's real api using useEffect - learnt useEffect - added Shimmer effect - added Search functionality - React rendering and re-rendering process

---
+++ Episode-06 Exploring the world/src/components/Body.js
@@ -0,0 +1,85 @@
+import RestaurantCard from "./RestaurantCard";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
+const Body = () => {
+  // Local State variable - Super powerful variable
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [listOfFilteredRestaurants, setListOfFilteredRestaurants] = useState(
+    []
+  );
+
+  // Whenever state variables update, react triggers a reconciliation cycle(re-renders the component)
+  console.log("Body Rendered");
+
+  useEffect(() => {
+    console.log("useeffect rendered");
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    // Optional Chaining
+    json?.data?.cards[2]?.cardType === "seeAllRestaurants"
+      ? setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+      : setListOfRestaurants(json?.data?.cards[0]?.data?.data?.cards);
+
+    json?.data?.cards[2]?.cardType === "seeAllRestaurants"
+      ? setListOfFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+      : setListOfFilteredRestaurants(json?.data?.cards[0]?.data?.data?.cards);
+  };
+
+  // Conditional Rendering
+  // if (listOfRestaurants.length === 0) {
+  //   return <Shimmer />;
+  // }
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              let filteredList = listOfRestaurants.filter((res) =>
+                res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setListOfFilteredRestaurants(filteredList);
+              setSearchText("");
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfFilteredRestaurants?.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
